b5e704b693b6411d06ef90c189e346fb15f9e329
Timur4ik <tim.skobenko@mail.ru>
2025-04-16 19:19:40 +0300

    feat: add greating

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Напиши своё имя: ");
+string some = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine($"Привет, {some}.");
